7e3634eb5b220f5449d96dbad5bf7ea6eaf79fc2
your_username <stztrujillo@hotmail.com>
2023-11-17 19:31:21 +0000

    implemented contract

--- app-one/contracts/BuyMeACoffee.sol
+++ app-one/contracts/BuyMeACoffee.sol
@@ -2,7 +2,25 @@
 pragma solidity ^0.8.9;
 
 contract Coffee { 
+    address public owner;
+    string public lastMessage;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function sendFundAndMessage(string memory msg) external payable {
+        require(msg.value > 0, "Please send some funds");
+        lastMessage = msg;
+        // Perform any additional logic or emit events as needed
+    }
+
+    function getLastMessage() external view returns (string memory) {
+        return lastMessage;
+    }
 
 
-    
+
+
+
 }
