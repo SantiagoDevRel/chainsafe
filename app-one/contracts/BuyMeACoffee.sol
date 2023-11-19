6b9a853ce49b9efe032cc5bab85c513b12330576
your_username <stztrujillo@hotmail.com>
2023-11-19 10:02:52 +0000

    fixed typo

--- app-one/contracts/BuyMeACoffee.sol
+++ app-one/contracts/BuyMeACoffee.sol
@@ -2,17 +2,16 @@
 pragma solidity ^0.8.9;
 
 contract Coffee { 
-    address public owner;
-    string public lastMessage;
+    address private owner;
+    string private lastMessage;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function sendFundAndMessage(string memory msg) external payable {
+    function sendFundAndMessage(string memory _msg) external payable {
         require(msg.value > 0, "Please send some funds");
-        lastMessage = msg;
-        // Perform any additional logic or emit events as needed
+        lastMessage = _msg;
     }
 
     function getLastMessage() external view returns (string memory) {
@@ -23,6 +22,6 @@ contract Coffee {
         require(msg.sender == owner, "Only owner can withdraw the funds");
         uint256 _balance = address(this).balance;
         (bool success,) = payable(owner).call{value: _balance}("");
-        require(sucess, "Transfer failed");
+        require(success, "Transfer failed");
     }
 }
