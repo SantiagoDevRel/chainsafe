c7db5412ccb83ebb23ceefe82182c958340c3599
your_username <stztrujillo@hotmail.com>
2023-11-19 12:06:51 +0000

    initialize wallet

--- app-one/scripts/deploy.js
+++ app-one/scripts/deploy.js
@@ -1,32 +1,33 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+require("dotenv").config();
+const { Web3 } = require("web3");
+const { abi, bytecode } = require("../src/artifacts/contracts/BuyMeACoffee.sol/Coffee.json");
+const web3 = new Web3(process.env.sepolia);
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  //initialize wallet
+  const [account] = web3.eth.accounts.wallet.add("0x" + process.env.pk);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  //initialize contract
+  const Contract = new web3.eth.Contract(abi);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
+  //arguments
+  const args = "This is the first msg";
+
+  //create deploy transaction
+  const contract = Contract.deploy({
+    data: bytecode,
+    arguments: [args],
   });
 
-  await lock.waitForDeployment();
+  //make tx
+  const tx = await contract.send({
+    from: account.address,
+  });
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  //print deployed address
+  console.log("Done", tx.options.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
