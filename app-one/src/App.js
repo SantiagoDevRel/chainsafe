4eb4b859c5d6f8e3ba3bdfaa03b3b53d704bf603
your_username <stztrujillo@hotmail.com>
2023-11-19 12:07:17 +0000

    added contract interaction scripts

--- app-one/src/App.js
+++ app-one/src/App.js
@@ -1,35 +1,52 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Web3 from "web3";
+import artifacts from "./artifacts/contracts/BuyMeACoffee.sol/Coffee.json";
 
 function App() {
-  let web3 = {};
+  let web3 = {},
+    contractAddress = "0x64c68599D8dC48DEb2653d11bc64B8BDA6948Ce4";
   const [connectedWallet, setConnectedWallet] = useState(null);
   const [message, setMessage] = useState("");
   const [donation, setDonation] = useState("");
   const [lastMessage, setLastMessage] = useState("");
+  const [contract, setContract] = useState({});
 
   async function connectWallet() {
     if (window.ethereum) {
       web3 = new Web3(window.ethereum);
+      console.log("web3", web3);
       //request accounts (connect metamask)
-      await window.ethereum.request({ method: "eth_accounts" });
+      await window.ethereum.request({ method: "eth_requestAccounts" });
 
-      //return array with the connected accounts from metamask
-      //store the first account
       const accounts = await web3.eth.getAccounts();
       setConnectedWallet(accounts[0]);
+      initializeContract();
     }
   }
 
-  async function sendFundsAndMessage(msg, dnt) {
-    console.log("Sending message:", msg, "Donation:", dnt);
-    // TODO: Add logic to send funds and message to the contract
+  async function initializeContract() {
+    setContract(new web3.eth.Contract(artifacts.abi, contractAddress, { gas: "123", dataInputFill: "data" }));
   }
 
   async function getMessage() {
-    setLastMessage("Last message");
-    // TODO: Call getMessage()
+    const lastMsg = await contract.methods.getLastMessage().call();
+    setLastMessage(lastMsg);
+  }
+
+  async function sendFundsAndMessage(msg, val) {
+    if (msg.trim() == "" || val.trim() == "" || val.trim() <= 0) {
+      alert("Please send funds and write a message :)");
+    } else {
+      try {
+        console.log("message:", msg, "Donation:", val);
+        const tx = await contract.methods.sendFundAndMessage(msg).send({ from: connectedWallet, value: val });
+        alert(`Success, Tx hash: ${tx.transactionHash}`);
+        await getMessage();
+      } catch (error) {
+        alert("User rejected tx");
+      }
+    }
   }
 
   return (
@@ -44,7 +61,7 @@ function App() {
         </button>
       )}
       <input type="text" placeholder="Leave a message..." value={message} onChange={(e) => setMessage(e.target.value)} className="message-input" />
-      <input type="number" placeholder="Enter donation amount" value={donation} onChange={(e) => setDonation(e.target.value)} min={0} className="donation-input" />
+      <input type="number" placeholder="Enter donation amount in wei" value={donation} onChange={(e) => setDonation(e.target.value)} min={0} className="donation-input" />
       <button onClick={() => sendFundsAndMessage(message, donation)} className="buy-coffee-button">
         Buy Me a Coffee ☕
       </button>
